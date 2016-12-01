6b9858514b423f508767787fbff210b585a31963
kjacks <kjack227@gmail.com>
2016-12-01 15:58:23 -0500

    built skeleton for state profile pages

--- src/components/StatePage.js
+++ src/components/StatePage.js
@@ -3,7 +3,9 @@
 import React from 'react';
 import { Link } from 'react-router';
 import NotFoundPage from './NotFoundPage';
-import statesMenu from './statesMenu';
+import SectionNav from './SectionNav.jsx';
+import ProfileSection from './ProfileSection.jsx';
+
 import Medal from './Medal';
 import Flag from './Flag';
 import $ from 'jquery';
@@ -48,13 +50,32 @@ export default class StatePage extends React.Component {
       return <NotFoundPage/>;
     }
     return (
-      <div className="state-full">
-        <div className="state">
-          <header />
-          <div className="picture-container">
-            <h2 className="name">{stateName.name}</h2>
-          </div>
+      <div className="location-profile state">
+        <div className="location-profile__title-container">
+          <h2 className="location-profile__title">{stateName.name}</h2>
         </div>
+        <SectionNav />
+        <ProfileSection 
+          title="Students" 
+          subtitle="Student data is collected from the Integrated Postsecondary Education Data System (IPEDS)"
+        />
+        <ProfileSection 
+          title="Loans" 
+          subtitle="Student data is collected from the Integrated Postsecondary Education Data System (IPEDS)"
+        />
+        <ProfileSection 
+          title="Grants" 
+          subtitle="Student data is collected from the Integrated Postsecondary Education Data System (IPEDS)"
+        />
+        <ProfileSection 
+          title="Schools" 
+          subtitle="Student data is collected from the Integrated Postsecondary Education Data System (IPEDS)"
+        />
+        <ProfileSection 
+          title="Outcomes" 
+          subtitle="Student data is collected from the Integrated Postsecondary Education Data System (IPEDS)"
+        />
+
         <div className="navigateBack">
           <Link to="/">« Back to the index</Link>
         </div>
