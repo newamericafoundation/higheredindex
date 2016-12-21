c1db277d9199ea8b3ce33a3e3dddf591436a3462
kjacks <kjack227@gmail.com>
2016-12-21 12:39:56 -0800

    implemented sidemenu layout and topnav layout

---
+++ src/components/SideMenuLayout.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router';
+import TopNav from './TopNav.jsx';
+import SideMenu from './SideMenu';
+import { toggleMenuExpansion } from '../actions'
+
+class SideMenuLayout extends React.Component {
+  render() {
+    let contentClasses = "main-content";
+    contentClasses += this.props.contentShifted ? " shifted" : "";
+    return (
+      <div className="app-container">
+        <SideMenu />
+
+        <div className={contentClasses}>
+          {this.props.children}
+        </div>
+        <div className="main-content__overlay" onClick={this.props.clearSidemenu} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    contentShifted: state.menuExpanded
+  }
+}
+
+const mapDispatchToProps = (dispatch) => { 
+  return {
+    clearSidemenu: () => {
+      console.log("clicked");
+      dispatch(toggleMenuExpansion(false))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SideMenuLayout)
