1749eb8a5ea8bdb099073a8224edfea51bed5267
kjacks <kjack227@gmail.com>
2017-01-11 12:28:17 -0500

    modified data block paragraph to find current year for data

--- src/components/stVizSettings.js
+++ src/components/stVizSettings.js
@@ -2,7 +2,7 @@ const stVizSettings = [
 	{
 		title: "Enrollment by Student Type",
 		paragraphSettings: {
-			textSections: ['', 'institutions enrolled', 'undergraduate students and', 'graduate students;', 'full-time students and', 'part-time-students; and', 'adult education students in @year'],
+			textSections: ['', ' institutions enrolled ', ' undergraduate students and ', ' graduate students; ', ' full-time students and ', ' part-time-students; and ', ' adult education students in @year'],
 			variables: ['name', 'data', 'other_data', 'data', 'other_data', 'data']
 		},
 		vizSettings: {
@@ -17,7 +17,7 @@ const stVizSettings = [
 	{
 		title: "Enrollment by Gender",
 		paragraphSettings: {
-			textSections: ['', 'institutions enrolled', 'undergraduate students and', 'graduate students;', 'full-time students and', 'part-time-students; and', 'adult education students in @year'],
+			textSections: ['', ' institutions enrolled ', ' undergraduate students and ', ' graduate students; ', ' full-time students and ', ' part-time-students; and ', ' adult education students in @year'],
 			variables: ['name', 'data', 'other_data', 'data', 'other_data', 'data']
 		},
 		vizSettings: {
