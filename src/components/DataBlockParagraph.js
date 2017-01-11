a0d17605fff9520c2a1438ea38659fd716dbe1f8
kjacks <kjack227@gmail.com>
2017-01-11 12:28:17 -0500

    modified data block paragraph to find current year for data

--- src/components/DataBlockParagraph.js
+++ src/components/DataBlockParagraph.js
@@ -1,31 +1,44 @@
 import React from 'react';
 var d3 = require("d3");
 
+function getMaxYear(variables, data) {
+	let totalMaxYear = 0;
+	for (let variable of variables) {
+		if (typeof(data[variable]) == 'object') {
+			let keys = Object.keys(data[variable]);
+			let localMaxYear = d3.max(keys, (d) => { return Number(d) });
+			totalMaxYear = localMaxYear > totalMaxYear ? localMaxYear : totalMaxYear;
+		}
+	}
+
+	return totalMaxYear;
+}
+
+
 export default function DataBlockParagraph(props) {
 	const {settings, data} = props,
 		{textSections, variables} = settings;
 	console.log(textSections, variables, data);
   	
-  	let populatedText = [];
+  	let populatedText = [],
+  		totalMaxYear = getMaxYear(variables, data);
 
 	textSections.map((text, i) => {
 		let variable = variables[i],
 			variableClass = variable == 'name' ? '' : "data-block__paragraph__data";
-			
-		populatedText.push(<span> {text} </span>);
+		text = text.replace("@year", totalMaxYear);
+		populatedText.push(<span>{text}</span>);
 
 		if (typeof(data[variable]) == 'object') {
 			let keys = Object.keys(data[variable]);
-			let maxYear = d3.max(keys, (d) => { return Number(d) });
-
-			populatedText.push(<span className={variableClass}>{data[variable][maxYear]}</span>);
+			let localMaxYear = d3.max(keys, (d) => { return Number(d) });
+			populatedText.push(<span className={variableClass}>{data[variable][localMaxYear]}</span>);
 		} else {
 			populatedText.push(<span className={variableClass}>{data[variable]}</span>);
 		}
-		
     })
 
-	console.log(populatedText);
+    console.log(populatedText);
     return (
       <div className="data-block__paragraph">
       	<p>{populatedText}</p>
