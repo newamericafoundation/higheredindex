6d3b7c1a6cb7266184b3111f93259913ff3288cd
kjacks <kjack227@gmail.com>
2017-01-12 16:50:05 -0500

    started implementing abstract simple chart

--- src/components/DataBlockViz.js
+++ src/components/DataBlockViz.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import LineChart from "./LineChart";
+import AbstractSimpleChart from "./AbstractSimpleChart";
 import $ from 'jquery';
 
 
@@ -10,18 +11,18 @@ export default class DataBlockViz extends React.Component {
   	const {settings, data} = this.props,
       {type} = settings;
 
-    let viz;
-    switch (type) {
-      case "line-chart":
-        viz = <LineChart settings={settings} data={data} />
-        break;
-      default:
-        viz = "No Chart Type";
-    }
+    // let viz;
+    // switch (type) {
+    //   case "line-chart":
+    //     viz = <LineChart settings={settings} data={data} />
+    //     break;
+    //   default:
+    //     viz = "No Chart Type";
+    // }
 
     return (
     	<div className="data-block__viz">
-    		{viz}
+    		<AbstractSimpleChart settings={settings} data={data} />
     	</div>
     )
   }
