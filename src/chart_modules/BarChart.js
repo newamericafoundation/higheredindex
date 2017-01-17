dec05349b874e777ab4717b69a43434d3579abe9
kjacks <kjack227@gmail.com>
2017-01-17 17:09:34 -0500

    finished bar chart initial build

--- src/chart_modules/BarChart.js
+++ src/chart_modules/BarChart.js
@@ -11,15 +11,18 @@ export default class LineChart {
 		this.mouseoverFunc = mouseoverFunc;
 		this.mouseoutFunc = mouseoutFunc;
 
-		this.initializeXAxis();
-		this.initializeDataLines();
-	}
+		this.initializeXScale();
+		this.initializeDataBars();
 
-	initializeXAxis() {
-		this.xAxis = this.domElem.append("g")
+        this.xAxis = this.domElem.append("g")
             .attr("class", "axis axis--x");
 
-        this.x = d3.scaleLinear();
+	}
+
+	initializeXScale() {
+        this.x = d3.scaleBand()
+            .paddingInner(0.3)
+            .paddingOuter(0.4);
 
         let keyList = [];
 
@@ -27,26 +30,20 @@ export default class LineChart {
             let keys = Object.keys(this.data[variable.variable]);
             keyList.push(...keys);
         }
-        // de-duplication
+
         keyList = Array.from(new Set(keyList));
 
-        let xExtents = d3.extent(keyList)
-        this.x.domain([Number(xExtents[0]) - .15 , Number(xExtents[1]) + .15]);
+        this.x.domain(keyList);
 	}
 
-	initializeDataLines() {
-		this.dataLines = {};
-		this.dataCircles = {};
-
+	initializeDataBars() {
+		this.dataBars = {};
+        
+         let dataArray;
 		for (let variable of this.variables) {
 			let varName = variable.variable;
-			this.dataLines[varName] = this.domElem.append("path")
-                .attr("class", "line-chart__data-line")
-                .style("fill", "none")
-                .style("stroke", variable.color)
-                .style("stroke-width", "1.5px");
 
-            let dataArray = Object.keys(this.data[varName]).map(
+            dataArray = Object.keys(this.data[varName]).map(
             	(key) => {
             		let year = key,
             			value = this.data[varName][key];
@@ -54,29 +51,25 @@ export default class LineChart {
 				}
 			)
 
-            this.dataCircles[varName] = this.domElem.selectAll("circle#" + varName)
+            this.dataBars[varName] = this.domElem.selectAll("rect#" + varName)
             	.data(dataArray)
-              .enter().append("circle")
-              	.attr("id", varName)
-              	.attr("class", "line-chart__data-circle")
-                .attr("r", 4)
-                .style("stroke", variable.color)
-                .style("stroke-width", "1.5px")
+              .enter().append("rect")
+              	.attr("id", (d) => { console.log(d); return varName; })
+              	.attr("class", "bar-chart__data-bar")
+                .style("fill", variable.color)
+                .style("stroke", "white")
+                .style("stroke-width", "1px")
                 .on("mouseover", (d, index, paths) => { return this.mouseoverFunc(d, paths[index], d3.event, varName); })
                 .on("mouseout", () => this.mouseoutFunc());
-		}
+        }
     }
 
     update(updateParams) {
+        const {width, height} = updateParams;
+ 		this.updateXScale(updateParams);
+ 		this.updateDataBars(updateParams);
 
- 		this.updateXAxis(updateParams);
- 		this.updateDataLines(updateParams);
-    }
-
-    updateXAxis(updateParams) {
-    	const {width, height} = updateParams;
-
-    	this.xAxis
+        this.xAxis
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(this.x).tickSize(0).tickPadding(10)
                 .tickFormat(function(e){
@@ -87,43 +80,45 @@ export default class LineChart {
                     return e;
                 })
             );
-
-        this.x.range([0, width]);
     }
 
-    updateDataLines(updateParams) {
+    updateXScale(updateParams) {
+    	const {width, height} = updateParams;
+
+        this.x.rangeRound([0, width]);
+        
+    }
+
+    updateDataBars(updateParams) {
         const {y, width, height, currHovered, valsShown} = updateParams;
 
-        const getLine = (dataObject) => {
-          let line = d3.line()
-            .x(d => {return this.x(d); })
-            .y(d => {return y(dataObject[d]); });
+        console.log(currHovered);
 
-          return line(Object.keys(dataObject));
+        let barHeights = {};
+        for (let year of this.x.domain()) {
+            barHeights[year] = height;
         }
 
         for (let variable of this.variables) {
-        	let varName = variable.variable;
-				this.dataLines[varName]
-                	.attr("d", getLine(this.data[varName]))
-                	.classed("disabled", valsShown.indexOf(varName) == -1)
+         let varName = variable.variable;
 
-                this.dataCircles[varName]
-                	.attr("cx", (d) => { return this.x(d.year)})
-                	.attr("cy", (d) => { return y(d.value)})
-                	.classed("disabled", valsShown.indexOf(varName) == -1)
-                	.attr("fill", (d) => {
-                		if (currHovered) {
-                			if (varName == currHovered.varName && d.year == currHovered.year) {
-                				return variable.color;
-                			}
-                		} 
-                		return "white";
-                	})
+            this.dataBars[varName]
+                .attr("x", (d) => { return this.x(d.year); })
+                .attr("y", (d) => {
+                    barHeights[d.year] -= (height - y(d.value));
+                    return barHeights[d.year];
+                })
+                .attr("height", (d) => { return height - y(d.value); })
+                .attr("width", this.x.bandwidth())
+                .attr("opacity", (d) => {
+                     if (currHovered) {
+                         if (varName == currHovered.varName && d.year == currHovered.year) {
+                             return .7;
+                         }
+                     } 
+                     return 1;
+                })
+                .classed("disabled", valsShown.indexOf(varName) == -1)
         }
-        if (currHovered && currHovered.component) {
-	        d3.select(currHovered.component)
-	        	.attr("fill", "green")
-	    }
     }
 }
